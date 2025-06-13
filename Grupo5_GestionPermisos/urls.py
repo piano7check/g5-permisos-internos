e487c94903bfd0bf7445110fc4afb73b6865817a
R-Duran19 <ricardoduran25a@gmail.com>
2025-06-12 23:54:40 -0400

    arreglo de mas colores, fixeo de la autentificacion por google

--- Grupo5_GestionPermisos/urls.py
+++ Grupo5_GestionPermisos/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from a_permissions.views import HomeView
 
 urlpatterns = [
+
     path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
